cbdddbfbe152e7467c2eaf4ad26f365990b645a4
Emre Tuna <tunaemre@windowslive.com>
2022-08-25 18:13:01 +0300

    wp install file changed

--- config/install_wp.php
+++ config/install_wp.php
@@ -1,19 +1,20 @@
 <?php
 
 /*
- * @package     Extly.VM
+ * @package     Lando.VM
  *
- * @author      Extly, CB. <team@extly.com>
- * @copyright   Copyright (C) 2012-2020 Extly, CB <team@extly.com>
+ * @author      Lando, CB. <admin@wordpress.lndo.site>
+ * @copyright   Copyright (C) 2012-2020 Lando, CB <admin@wordpress.lndo.site>
  * @license     MIT
  *
- * @see         https://www.extly.com
+ * @see         https://www.wordpress.lndo.site
  */
 
 $landoInfo = json_decode(getenv('LANDO_INFO'), true);
 $landoWebroot = getenv('LANDO_WEBROOT');
 
-$url = $landoInfo['appserver']['urls'][0];
+$url = $landoInfo['appserver']['urls'][1];
+echo $url;
 
 $dbServer = $landoInfo['database']['internal_connection']['host'];
 $dbName = $landoInfo['database']['creds']['database'];
@@ -46,9 +47,9 @@ $cmd = [
     'wp core install',
     '--url="'.$url.'"',
     '--title="Test Blog Title"',
-    '--admin_user="team@extly.com"',
-    '--admin_password="team@extly.com"',
-    '--admin_email="team@extly.com"',
+    '--admin_user="admin"',
+    '--admin_password="password"',
+    '--admin_email="admin@wordpress.lndo.site"',
 
 ];
 $installScript = implode(' ', $cmd);
